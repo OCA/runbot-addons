253f3facdb8efa7a687081f567c2da9241c4da54
Sandy Carter <sandy.carter@savoirfairelinux.com>
2014-06-24 11:12:54 -0400

    Improve runbot_custom description

--- runbot_custom/__openerp__.py
+++ runbot_custom/__openerp__.py
@@ -28,7 +28,17 @@
     'description': """
 Runbot Custom Builder
 =====================
-Runbot with custom build instructions
+
+Runbot with custom build instructions.
+This is useful for custom installation and deployment scripts such as buildout.
+
+Add option in repo form view for custom builds. When checked:
+
+* Modules to test becomes required attribute.
+* Custom build directory can be specified. This is where the repo will be cloned in the build directory.
+* Custom server path is required to be specified. This is tells runbot which executable script to run.
+* Custom server flags can be specified to add to execution. E.g. --workers=0
+* Pre-build commands can be run such as additional fetch scripts or buildout.
 
 Contributors
 ------------
