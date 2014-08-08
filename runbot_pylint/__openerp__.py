f7665c2d103f75274dda885ddcc87b34c108df55
Luis Ernesto García Medina <ernesto_gm@vauxoo.com>
2014-08-08 08:38:36 -0500

    add runbot_pylint module for development review guidelines

---
+++ runbot_pylint/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Runbot Pylint',
+    'category': 'Website',
+    'summary': 'Runbot',
+    'version': '1.0',
+    'description': "Runbot",
+    'author': 'OpenERP SA',
+    'depends': ['runbot'],
+    'external_dependencies': {
+    },
+    'data': [
+        "view/runbot_pylint_view.xml"
+    ],
+    'installable': True,
+}
