786442483a79c2b120158d4d502f56f25b324cea
moylop260 <moylop260@vauxoo.com>
2015-07-16 23:49:55 +0000

    [ADD] runbot_pylint: Add module to run pylint checks in runbot

---
+++ runbot_pylint/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+##############################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: moylop260@vauxoo.com
+############################################################################
+
+from . import runbot_repo
+from . import runbot_build
