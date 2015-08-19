df8a13a74546efd6124ce29d572ed11580b1ba52
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-08-19 13:48:53 +0200

    create README.rst, document the other repo feature

--- runbot_build_instructions/__openerp__.py
+++ runbot_build_instructions/__openerp__.py
@@ -24,28 +24,7 @@
     'name': 'Runbot Custom Build and Run Instructions',
     'category': 'Website',
     'summary': 'Runbot with custom build and run instructions',
-    'version': '1.1',
-    'description': """
-Runbot Custom Build and Run Instructions
-========================================
-
-Runbot with custom build instructions.
-This is useful for custom installation and deployment scripts such as buildout.
-
-Add option in repo form view for custom builds. When checked:
-
-* Modules to test becomes required attribute.
-* Custom build directory can be specified. This is where the repo will be
-  cloned in the build directory.
-* Custom server path is required to be specified. This is tells runbot which
-  executable script to run.
-* Custom server flags can be specified to add to execution. E.g. --workers=0
-* Pre-build commands can be run such as additional fetch scripts or buildout.
-
-Contributors
-------------
-* Sandy Carter (sandy.carter@savoirfairelinux.com)
-""",
+    'version': '1.2',
     'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
     'depends': ['runbot'],
     'data': [
