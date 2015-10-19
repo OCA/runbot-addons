a4c5eb1325f99895070b8acf48baaa0feb8c085e
Moises Lopez <moylop260@vauxoo.com>
2015-10-19 02:10:17 +0000

    [ADD] runbot_selftest: Module to enable self test of runbot

---
+++ runbot_selftest/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2015 Vauxoo
+# Coding by Moises Lopez <moylop260@vauxoo.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Runbot self test",
+    "summary": "Runbot self test",
+    "version": "8.0.1.0.0",
+    "category": "Runbotd",
+    "website": "https://odoo-community.org/",
+    "author": "Vauxoo, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "runbot",
+    ],
+    "demo": [
+        'demo/runbot_demo.xml',
+    ],
+}
