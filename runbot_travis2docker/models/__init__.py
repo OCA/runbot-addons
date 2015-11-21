181afcc07e639e6e593fa97786fc3e168139127c
Moises Lopez <moylop260@vauxoo.com>
2015-11-21 18:03:46 -0500

    [ADD] runbot_travis2docker: Add runbot module to work with travis2docker

---
+++ runbot_travis2docker/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# © 2015 Vauxoo
+#   Coded by: moylop260@vauxoo.com
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import runbot_build
+from . import runbot_repo
