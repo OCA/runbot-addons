27d41fb4f6821e5ffca7bb73530f8dcac49223b1
Moises Lopez <moylop260@vauxoo.com>
2015-11-21 18:03:46 -0500

    [ADD] runbot_travis2docker: Add runbot module to work with travis2docker

---
+++ runbot_travis2docker/__openerp__.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+# © 2015 Vauxoo
+#   Coded by: moylop260@vauxoo.com
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Runbot travis to docker",
+    "summary": "Generate docker with odoo instance based on .travis.yml",
+    "version": "9.0.1.0.0",
+    "category": "runbot",
+    "website": "https://odoo-community.org/",
+    "author": "Vauxoo,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "runbot",
+    ],
+    "external_dependencies": {
+        "python": [
+            'travis2docker',
+        ],
+        'bin': [
+            'docker',
+        ],
+    },
+    "data": [
+        "views/runbot_repo_view.xml",
+    ],
+    "demo": [
+        "demo/runbot_repo_demo.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
