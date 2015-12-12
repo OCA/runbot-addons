465d3253a8c5aac68af657f4d1c1cae4faa00c12
Holger Brunn <hbrunn@therp.nl>
2015-12-12 20:54:19 +0100

    runbot_website_display

---
+++ runbot_website_display/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Runbot display options",
+    "version": "8.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Usability",
+    "summary": "Allows to reorder and hide runbot repositories",
+    "depends": [
+        'runbot',
+    ],
+    "data": [
+        "views/runbot_repo.xml",
+    ],
+    "installable": True,
+}
