a419eb4757d7fd966b05ab582bfd0a0a375d6195
Holger Brunn <hbrunn@therp.nl>
2016-11-27 14:35:22 +0100

    [ADD] runbot_coverage

---
+++ runbot_coverage/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Coverage for runbot",
+    "version": "8.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Runbot",
+    "summary": "Collect coverage information during test_all",
+    "depends": [
+        'runbot',
+    ],
+    "data": [
+        "views/runbot_repo.xml",
+        'views/templates.xml',
+    ],
+    "qweb": [
+    ],
+    "test": [
+    ],
+    "images": [
+    ],
+    "pre_init_hook": False,
+    "post_init_hook": False,
+    "uninstall_hook": False,
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "external_dependencies": {
+        'binary': [
+            'coverage',
+        ],
+    },
+}
