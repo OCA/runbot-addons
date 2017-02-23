e1d212800e78ab3687e10e9c39535e945d0a84d2
Jesus Zapata <jesus@vauxoo.com>
2017-02-23 00:42:59 -0400

    [ADD] runbot_gitlab_minimal: Gitlab supports with minimal changes (#90)
    Re-use all original method of runbot module

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright <2017> <Vauxoo info@vauxoo.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Runbot Gitlab Integration Minimal',
+    'summary': 'Gitlab Integration Minimal',
+    'author': 'Vauxoo, Odoo Community Association (OCA)',
+    'website': "http://www.vauxoo.com",
+    'license': 'AGPL-3',
+    'category': 'runbot',
+    'version': '9.0.1.1.0',
+    'depends': ['runbot'],
+    'data': [
+        'views/runbot_repo.xml',
+    ],
+    'installable': True,
+}
