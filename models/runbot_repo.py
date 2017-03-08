b58d49df331f79b4cc35cc33f33a42250ca9632d
Jesus Zapata <jesus@vauxoo.com>
2017-03-08 14:26:27 -0400

    [FIX] runbot_gitlab_minimal: Many fixes (#91)
    * [FIX] runbot_gitlab_minimal: Fix when the sha belongs to another repository
    
    * [ADD] runbot_gitlab_minimal: Gitlab supports with minimal changes (#90)
    
    Re-use all original method of runbot module
    
    [FIX] runbot_gitlab_minimal: Solved the problem by calculating Shah's father
    
    [FIX] runbot_gitlab_minimal: Call parent if repo not uses gitlab
    
    [FIX] runbot_gitlab_minimal: Fix link to merge request
    
    [FIX] runbot_gitlab_minimal: Fix when the branch name not is the merge request
    
    and support killed status in runbot
    
    [FIX] runbot_gitlab_minimal: Fix name of method computed
    
    [FIX] runbot_gitlab_minimal: Bad method name
    
    * [FIX] runbot_gitlab_minimal: methoh _get_branch_url now call the super method
    
    * [FIX] runbot_gitlab_minimal: Skip iteracion when is not gitlab_uses

--- models/runbot_repo.py
+++ models/runbot_repo.py
@@ -9,9 +9,28 @@ import urllib
 
 import requests
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 _logger = logging.getLogger(__name__)
+
+
+def _get_url(url, base):
+    match_object = re.search('([^/]+)/([^/]+)/([^/.]+(.git)?)', base)
+    if match_object:
+        project_name = (match_object.group(2) + '/' + match_object.group(3))
+        url = url.replace(':owner', match_object.group(2))
+        url = url.replace(':repo', match_object.group(3))
+        url = 'https://%s/api/v3%s' % (match_object.group(1), url)
+        url = url.replace('/repos/', '/projects/')
+        url = url.replace(project_name, urllib.quote(project_name, safe=''))
+    return url
+
+
+def _get_session(token):
+    session = requests.Session()
+    session.auth = (token, 'x-oauth-basic')
+    session.headers.update({'PRIVATE-TOKEN': token})
+    return session
 
 
 def custom_repo(func):
@@ -77,30 +96,14 @@ class RunbotRepo(models.Model):
             if not repo.token:
                 continue
             try:
-                match_object = re.search('([^/]+)/([^/]+)/([^/.]+(.git)?)',
-                                         repo.base)
-                if match_object:
-                    project_name = (match_object.group(2) + '/' +
-                                    match_object.group(3))
-                    url = url.replace(':owner', match_object.group(2))
-                    url = url.replace(':repo', match_object.group(3))
-                    url = 'https://%s/api/v3%s' % (match_object.group(1),
-                                                   url)
-                    url = url.replace('/repos/', '/projects/')
-                    url = url.replace(project_name,
-                                      urllib.quote(project_name, safe=''))
+                url = _get_url(url, repo.base)
+                if url:
                     if '/pulls/' in url:
                         urls = url.split('/pulls/')
                         url = urls[0] + '/merge_requests?iid=' + urls[1]
                         is_url_merge = True
-                    session = requests.Session()
-                    session.auth = (repo.token, 'x-oauth-basic')
-                    session.headers.update({'PRIVATE-TOKEN': repo.token})
+                    session = _get_session(repo.token)
                     if payload:
-                        if payload['state'] == 'pending':
-                            payload['state'] = 'running'
-                        if payload['state'] in ('error', 'failure'):
-                            payload['state'] = 'failed'
                         response = session.post(url, data=payload)
                     else:
                         response = session.get(url)
@@ -117,3 +120,90 @@ class RunbotRepo(models.Model):
                                       payload)
                 else:
                     raise
+
+
+class RunbotBuild(models.Model):
+    _inherit = "runbot.build"
+
+    def github_status(self, cr, uid, ids, context=None):
+        runbot_domain = self.pool['runbot.repo'].domain(cr, uid)
+        for build in self.browse(cr, uid, ids, context=context):
+            is_merge_request = build.branch_id.branch_name.isdigit()
+            source_project_id = False
+            _url = _get_url('/projects/:owner/:repo/statuses/%s' % build.name,
+                            build.repo_id.base)
+            if not build.repo_id.uses_gitlab:
+                super(RunbotBuild, self).github_status(cr, uid, ids,
+                                                       context=context)
+                continue
+            if not build.repo_id.token:
+                continue
+            session = _get_session(build.repo_id.token)
+            try:
+                if is_merge_request:
+                    url = _get_url('/projects/:owner/:repo/merge_requests/'
+                                   '?iid=%s' % build.branch_id.branch_name,
+                                   build.repo_id.base)
+                    response = session.get(url)
+                    response.raise_for_status()
+                    json = response.json()[0]
+                    source_project_id = json['source_project_id']
+                    if source_project_id:
+                        url = _get_url('/projects/%s' % source_project_id,
+                                       build.repo_id.base)
+                        response = session.get(url)
+                        response.raise_for_status()
+                        json = response.json()
+                        base_url = (json['web_url'].replace('http://', '').
+                                    replace('https://', ''))
+                        _url = _get_url('/projects/:owner/:repo/statuses/%s' %
+                                        build.name, base_url)
+                desc = "runbot build %s" % (build.dest,)
+                if build.state == 'testing':
+                    state = 'running'
+                elif build.state in ('running', 'done'):
+                    state = 'failed'
+                    if build.result == 'ok':
+                        state = 'success'
+                    if build.result == 'ko':
+                        state = 'failed'
+                    if build.result == 'skipped':
+                        state = 'canceled'
+                    if build.result == 'killed':
+                        state = 'canceled'
+                    desc += " (runtime %ss)" % (build.job_time,)
+                else:
+                    continue
+                status = {
+                    "state": state,
+                    "target_url": "http://%s/runbot/build/%s" % (runbot_domain,
+                                                                 build.id),
+                    "description": desc,
+                    "context": "ci/runbot"
+                }
+                _logger.debug("gitlab updating status %s to %s", build.name,
+                              state)
+                response = session.post(_url, status)
+                response.raise_for_status()
+            except Exception:
+                _logger.exception('gitlab API error %s', url)
+
+
+class RunbotBranch(models.Model):
+    _inherit = "runbot.branch"
+
+    branch_url = fields.Char(compute='_get_branch_url')
+
+    @api.multi
+    def _get_branch_url(self):
+        _branch_urls = super(RunbotBranch, self)._get_branch_url(None, None)
+        for branch in self:
+            if not branch.repo_id.uses_gitlab:
+                branch.branch_url = _branch_urls[branch.id]
+            else:
+                if branch.branch_name.isdigit():
+                    branch.branch_url = "https://%s/merge_requests/%s" % (
+                        branch.repo_id.base, branch.branch_name)
+                else:
+                    branch.branch_url = ("https://%s/tree/%s" % (
+                        branch.repo_id.base, branch.branch_name))
