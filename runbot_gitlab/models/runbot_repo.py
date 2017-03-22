bfba68aa7e44880dce532fa84980fd174528375b
JesusZapata <jesus@vauxoo.com>
2017-03-22 19:40:34 +0000

    [IMP] runbot_gitlab: Convert url users/User/keys from github to gitlab

--- runbot_gitlab/models/runbot_repo.py
+++ runbot_gitlab/models/runbot_repo.py
@@ -18,12 +18,24 @@ _logger = logging.getLogger(__name__)
 def _get_url(url, base):
     match_object = re.search('([^/]+)/([^/]+)/([^/.]+(.git)?)', base)
     if match_object:
+        """When get is URL_GITHUB/api/v3/User/keys must be convert to
+        URL_GITLAB/User.keys Because the api of gitlab required admin token
+        for get the ssh keys
+        https://docs.gitlab.com/ee/api/users.html#list-ssh-keys"""
+        prefix = ('https://%s/api/v3%s'
+                  if not url.endswith('/keys') else 'https://%s%s')
         project_name = (match_object.group(2) + '/' + match_object.group(3))
         url = url.replace(':owner', match_object.group(2))
         url = url.replace(':repo', match_object.group(3))
-        url = 'https://%s/api/v3%s' % (match_object.group(1), url)
+        url = prefix % (match_object.group(1), url)
         url = url.replace('/repos/', '/projects/')
         url = url.replace(project_name, urllib.quote(project_name, safe=''))
+        if url.endswith('/keys'):
+            url = url.replace('users/', '').replace('/keys', '')
+            url = url + '.keys'
+        if '/pulls/' in url:
+            urls = url.split('/pulls/')
+            url = urls[0] + '/merge_requests?iid=' + urls[1]
     return url
 
 
@@ -88,33 +100,52 @@ class RunbotRepo(models.Model):
             input: URL_GITLAB/projects/... instead of URL_GITHUB/repos/...
             output: res['base']['ref'] = res['gitlab_base_mr']
                     res['head']['ref'] = res['gitlab_head_mr']
+        - Get user public keys
+            input: URL_GITLAB/User.keys... instead of
+                   URL_GITHUB/users/User/keys...
+            output: res['author'] = {'login': data['username']}
+                    res['commiter'] = {'login': data['username']}
         - Report statutes
             input: URL_GITLABL/... instead of URL_GITHUB/statuses/...
             output: N/A
         """
         for repo in self.browse(cr, uid, ids, context=context):
-            is_url_merge = False
             if not repo.token:
                 continue
             try:
                 url = _get_url(url, repo.base)
-                if url:
-                    if '/pulls/' in url:
-                        urls = url.split('/pulls/')
-                        url = urls[0] + '/merge_requests?iid=' + urls[1]
-                        is_url_merge = True
-                    session = _get_session(repo.token)
-                    if payload:
-                        response = session.post(url, data=payload)
-                    else:
-                        response = session.get(url)
-                    response.raise_for_status()
-                    json = response.json()
-                    if is_url_merge:
-                        json = json[0]
-                        json['head'] = {'ref': json['target_branch']}
-                        json['base'] = {'ref': json['source_branch']}
-                    return json
+                if not url:
+                    return
+                is_url_keys = url.endswith('.keys')
+                session = _get_session(repo.token)
+                if payload:
+                    response = session.post(url, data=payload)
+                else:
+                    response = session.get(url)
+                response.raise_for_status()
+                json = (response.json() if not is_url_keys
+                        else response._content)
+                if 'merge_requests?iid=' in url:
+                    json = json[0]
+                    json['head'] = {'ref': json['target_branch']}
+                    json['base'] = {'ref': json['source_branch']}
+                if '/commits/' in url:
+                    for own_key in ['author', 'committer']:
+                        key_email = '%s_email' % own_key
+                        if json[key_email]:
+                            url = _get_url('/users?search=%s' %
+                                           json[key_email],
+                                           repo.base)
+                            response = session.get(url)
+                            response.raise_for_status()
+                            data = response.json()
+                            json[own_key] = {
+                                'login':
+                                len(data) and data[0]['username'] or {}
+                            }
+                if is_url_keys:
+                    json = [{'key': ssh_rsa} for ssh_rsa
+                             in json.split('\n')]
             except Exception:
                 if ignore_errors:
                     _logger.exception('Ignored gitlab error %s %r', url,
