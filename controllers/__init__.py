c9e92a642d5b65e9ee0f4fb40e7c28d7336ab877
Jesus Zapata <jesus@vauxoo.com>
2017-06-08 09:41:36 -0400

    [REF] runbot_gitlab_minimal: Add webhook process fix #94 (#103)
    * [REF] runbot_gitlab_minimal: Add webhook process fix #94
    
    * [REF] runbot_gitlab_minimal: Delete repeated code and call the parent method
    
    * [REF] runbot_gitlab_minimal: Fingered error
    
    * [FIX] runbot_gitlab_minimal: Support two type of request json and text
    
    * [FIX] runbot_gitlab_minimal: Fix W601 and C1801 lint check

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,5 +2,4 @@
 # Copyright <2017> <Vauxoo info@vauxoo.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models
-from . import controllers
+from . import gitlab_ci_controller
