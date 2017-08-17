68080253a1a3d24b30f0d646d74aea81f8ba26b1
Dave Lasley <dave@laslabs.com>
2017-08-17 15:08:11 -0700

    [IMP] runbot_travis2docker: Add custom image * Add ability to define custom image for travis2docker builds * Default to standard `vauxoo/odoo-80-shippable` to make the image more obvious to user

--- runbot_travis2docker/models/runbot_repo.py
+++ runbot_travis2docker/models/runbot_repo.py
@@ -15,10 +15,17 @@ class RunbotRepo(models.Model):
 
     is_travis2docker_build = fields.Boolean('Travis to docker build')
     travis2docker_test_disable = fields.Boolean('Test Disable?')
+    travis2docker_image = fields.Char(
+        default=lambda s: s._default_travis2docker_image(),
+    )
     weblate_url = fields.Char(default="https://weblate.odoo-community.org/api")
     weblate_token = fields.Char()
     weblate_languages = fields.Char(help="List of code iso of languages E.g."
                                     " en_US,es_ES")
+
+    @api.model
+    def _default_travis2docker_image(self):
+        return 'vauxoo/odoo-80-image-shippable-auto'
 
     @api.multi
     @api.constrains('weblate_languages')
