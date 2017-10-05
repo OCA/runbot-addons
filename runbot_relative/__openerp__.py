1c47eddac7260a25f12c5f32c784d242e14e2333
Dave Lasley <dave@laslabs.com>
2017-10-04 22:47:20 -0700

    [MIG] runbot_secure: Upgrade to v9 (#87)
    * [MIG] runbot_secure: Upgrade to v9
    
    * [MIG] runbot_relative: Create new module to replace runbot_secure
    
    * [IMP] runbot_relative: Switch to JS override
    
    * Loosen the domain matching
    
    * ESLint

---
+++ runbot_relative/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Runbot Relative Links',
+    'category': 'Website',
+    'summary': 'Provide protocol relative links in Runbot',
+    'version': '9.0.1.0.0',
+    'author': "LasLabs, Odoo Community Association (OCA)",
+    'license': 'LGPL-3',
+    'depends': ['runbot'],
+    'data': [
+        'views/runbot_template.xml',
+        'views/assets.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ runbot_relative/static/src/js/runbot_relative.js
@@ -0,0 +1,41 @@
+/* Copyright 2017 LasLabs Inc.
+ * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+ */
+
+odoo.define('runbot_relative.runbot_relative', function(require){
+    "use strict";
+
+    var snippet_animation = require('web_editor.snippets.animation');
+
+    snippet_animation.registry.runbot_relative = snippet_animation.Class.extend({
+
+        selector: 'a',
+
+        start: function() {
+            var runbotHost = $('#runbot-host').val();
+            var elHref = this.$el.attr('href');
+            var elHost = this.getHostname(elHref);
+            var regex = new RegExp(runbotHost + '$');
+            if ( ! elHost.match(regex) ) {
+                // Host doesn't end with the correct domain
+                return;
+            }
+            this.$el.attr('href', elHref.replace(/https?:/, ''));
+        },
+
+        getHostname: function(uri) {
+            // Remove protocol
+            var split = uri.split('/');
+            var host = uri.indexOf('://') > -1
+                ? split[2]
+                : split[0];
+            // Remove port
+            host = host.split(':')[0];
+            // Remove query args
+            host = host.split('?')[0];
+            return host;
+        }
+
+    });
+
+});
