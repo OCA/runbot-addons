3a7c6bed93e4d12efddfe50f13aed31287690529
Holger Brunn <hbrunn@therp.nl>
2018-04-05 10:55:25 +0200

    [FIX] lints

--- runbot_gitlab/models/runbot_branch.py
+++ runbot_gitlab/models/runbot_branch.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2010 - 2014 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # Copyright 2017 Vauxoo <info@vauxoo.com> (<https://www.vauxoo.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -9,6 +8,7 @@ from openerp import models, fields, api
 class RunbotBranch(models.Model):
     _inherit = "runbot.branch"
 
+    # pylint: disable=method-compute
     branch_url = fields.Char(compute='_get_branch_url')
 
     @api.multi
