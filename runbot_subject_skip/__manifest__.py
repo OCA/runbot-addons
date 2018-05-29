219cd475a4af84713fb0c79706cb06ac5d1409d0
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2018-05-29 05:16:19 -0500

    Revert "[DEL] runbot_subject_skip: Deprecated because is now supported officially"
    This reverts commit 90306b086e5daa21cf7466b57dd97e823f754b2d.

---
+++ runbot_subject_skip/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright <2018> <Vauxoo info@vauxoo.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Runbot subject skip",
+    "summary": "Skip build if commit message has '[ci skip]'",
+    "version": "11.0.1.0.0",
+    "category": "runbot",
+    "website": "https://github.com/OCA/runbot-addons",
+    "author": "Vauxoo, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "runbot",
+    ],
+    "data": [
+    ],
+    "application": False,
+    "installable": True,
+}
